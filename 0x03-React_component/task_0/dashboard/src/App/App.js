1ae6bfeb7c58de5974b40682ffc2069f82dfbced
Abdulsamad <almuhandith1497@gmail.com>
2023-08-03 15:22:30 +0100

    changed function cmoponent to class component in task_0

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
@@ -8,31 +8,33 @@ import PropTypes from "prop-types";
 import "./App.css";
 import { getLatestNotification } from "../utils/utils";
 
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
+class App extends React.Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+  
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
 
-const listNotifications = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: getLatestNotification() },
-];
-
-class App extends Component (isLoggedIn) {
-  return (
-    <React.Fragment>
-      <div className="App">
-				<div className="App-head">
-					<Header />
-					<Notifications listNotifications={listNotifications} />
-				</div>
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+  render () {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <div className="App-head">
+            <Header />
+            <Notifications listNotifications={this.listNotifications} />
+          </div>
+          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 App.defaultProps = {
